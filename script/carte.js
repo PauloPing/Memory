e8b941fdb852475b986180f79d41265a367267f0
PauloPing <polo62.leblond@gmail.com>
2022-01-14 17:20:34 +0100

    feat game finish

--- script/carte.js
+++ script/carte.js
@@ -1,7 +1,31 @@
 class Game {
   
   arrayCase = [];
-  
+  nbReveal = 0;
+  caseReveal;
+
+  timer = 200;
+  timeout;
+  active = true;
+
+  /**
+   * timer for the game
+   */
+   goTimer(){
+    document.getElementById("memory__timerValue").textContent = this.timer+'s';
+    this.timer -= 1;
+    this.timeout = setTimeout(() => {
+      if(this.timer >= 0){
+        this.goTimer();
+      }else{
+        // verifGameWin();
+        document.getElementById("memory__result").textContent = "Désolé, c'est perdu!"
+        document.getElementById("memory__result").classList.add("loose");
+        this.active = false;
+      }
+    },1000);
+  }
+
   /**
   * @param {int} taille
   */
@@ -10,12 +34,9 @@ class Game {
     this.taille = taille;
     for (let i = 0; i < this.taille; i++) {
       this.arrayCase[i] = [];
-      // for (let j = 0; j < this.taille; j++) {
-      //   this.arrayCase[taille][j] = 0;
-      // }
     }
+    this.goTimer();
   }
-
     /**
   * @param {Case} caseItem 
   * @param {int} x 
@@ -26,17 +47,100 @@ class Game {
     // console.log(this.arrayCase);
   }
 
+  stopCompteur(){
+    clearTimeout(this.timeout);
+  }
+
+  reinit(){
+    for (let index = 0; index < this.arrayCase.length; index++) {
+      for (let j = 0; j < this.arrayCase.length; j++) {
+        this.arrayCase[index][j].hidden = false;
+      }
+    }
+    this.timer = 200;
+    this.active = true;
+    this.caseReveal = null;
+    this.nbReveal = 0;
+    clearTimeout(this.timeout);
+    this.goTimer();
+    console.log(this.arrayCase);
+  }
+  /**
+   * return nb card reveal.
+   * @returns {int}
+   */
+  returnNbReveal(){
+    return this.nbReveal;
+  }
+
+  /**
+   * edit value of nb reveal card.
+   * @param {int} x 
+   */
+  setReveal(x){
+    this.nbReveal = x;
+  }
+
   /**
    * @param {int} x 
    * @param {int} y 
+   * @returns {Case}
    */
   returnCase(x,y){
     return this.arrayCase[x][y];
   }
 
-  afficheArray(){
+/**
+ * Reverse two cases of game table    
+ * @param {int} x 
+ * @param {int} y 
+ * @param {int} x1 
+ * @param {int} y2 
+ */
+  reverseCase(x,y,x1,y2){
+    // console.log(x,y)
+    let tmpCase = this.returnCase(x,y);
+    tmpCase.editCoordinate(x1,y2);
+    this.arrayCase[x][y] = this.arrayCase[x1][y2];
+    this.arrayCase[x1][y2].editCoordinate(x,y);
+    this.arrayCase[x1][y2] = tmpCase;
+  }
+
+  /**
+   * 
+   * @returns {Case[][]}
+   */
+  returnArray(){
+    return this.arrayCase;
+  }
+
+  /**
+   * print a game table
+   */
+  printArray(){
     console.log(this.arrayCase);
   }
+
+  gameWin(){
+    return this.arrayCase.every((currentValue) => currentValue.every((current) => current.hidden));
+  }
+
+  /**
+   * return case reveal in table
+   * @returns {Case}
+   */
+  getCaseReveal(){
+    return this.caseReveal;
+  }
+
+  /**
+   * edit the reveal case
+   * @param {Case} revealCase 
+   */
+  editCaseReveal(caseReveal){
+    this.caseReveal = caseReveal;
+  }
+
 }
 
 class Case {
@@ -47,20 +151,44 @@ class Case {
  * @param {int} y 
  */
   constructor(name, x, y){
-    console.log("Case Init ("+x+","+y+")");
+
+    // console.log("Case Init ("+x+","+y+")");
     this.name = name;
+    this.x = x;
+    this.y = y;
+    this.hidden = false;
+  }
+
+  /**
+ * Edit coordinate of case
+ * @param {int} x 
+ * @param {int} y 
+ */
+  editCoordinate(x,y){
     this.x = x;
     this.y = y;
   }
 
+  /**
+   * 
+   * @returns {String}
+   */
   returnName(){
     return this.name;
   }
 
+  /**
+   * 
+   * @returns {int}
+   */
   returnX(){
     return this.x;
   }
 
+  /**
+   * 
+   * @returns {int}
+   */
   returnY(){
     return this.y;
   }
--- script/script.js
+++ script/script.js
@@ -1,29 +1,147 @@
-tailleJeu = 4;
-
+var tailleJeu = 4;
+var nbMix = 100;
+var jeu;
 /**
- * Create a lign inHTML page.
- * @param {Case[]} arrayCaseLigne 
+ * Create a lign inHTML page with hidden img.
+ * @param {Case[][]} arrayCaseLigne 
+ * @param {Game} jeu 
  */
-function createLigneHTML(arrayCaseLigne){
-
+function createLigneHTML(arrayCaseLigne, jeu){
+  var plateau = document.getElementById("memory__jeu");
+  let ligne;
+  let img;
+  for (let index = 0; index < this.tailleJeu; index++) {
+    ligne = document.createElement('div');
+    ligne.id = 'ligne'+index;
+    ligne.classList.add('ligne');
+    for(let i = 0; i < arrayCaseLigne.length; i++){
+      img = document.createElement('img');
+      img.src = './img/mem_back.jpg';
+      img.addEventListener('click', revealImage.bind(null, index,i,jeu), false);
+      img.classList.add('imgCase');
+      img.id = index+'-'+i;
+      ligne.appendChild(img);
+    }
+    plateau.appendChild(ligne);
+  }
 }
 
+/**
+ * Mix the array to play the game.
+ * @param {Game} jeu 
+ */
+function mixTable(jeu){
+  let nb1;
+  let nb2;
+  for (let index = 0; index < this.nbMix; index++) {
+    nb1 = Math.floor(Math.random() * (this.tailleJeu * this.tailleJeu));
+    nb2 = Math.floor(Math.random() * (this.tailleJeu * this.tailleJeu));
+    jeu.reverseCase(nb1 % this.tailleJeu, parseInt(nb1 / this.tailleJeu), nb2 % this.tailleJeu, parseInt(nb2 / this.tailleJeu));
+  }
+}
+
+/**
+ * Method to reveal img of card item. (onClick function)
+ * @param {int} x 
+ * @param {int} y 
+ * @param {Game} jeu 
+ */
+function revealImage(x,y,jeu){
+  if(jeu.active){
+    lastCase = jeu.returnCase(x,y);
+    if(!lastCase.hidden){
+      jeu.setReveal(jeu.returnNbReveal() + 1);
+      nbReveal = jeu.returnNbReveal();
+      if(nbReveal == 2 && jeu.caseReveal == lastCase){
+        jeu.setReveal(1);
+        return;
+      }
+      if(nbReveal <= 2){
+        document.getElementById(x+'-'+y).setAttribute('src', './img/'+jeu.returnCase(x,y).returnName());
+        if(nbReveal == 1){
+          jeu.editCaseReveal(jeu.returnCase(x,y));
+        }else{
+          setTimeout(() => {
+            caseReveal = jeu.caseReveal;
+            lastCase = jeu.returnCase(x,y);
+            if(lastCase.returnName() === caseReveal.returnName() && lastCase.hidden == false && caseReveal.hidden == false){
+              jeu.editCaseReveal(null);
+              lastCase.hidden = true;
+              caseReveal.hidden = true;
+              document.getElementById(caseReveal.returnX()+'-'+caseReveal.returnY()).classList.remove('imgCase')
+              document.getElementById(caseReveal.returnX()+'-'+caseReveal.returnY()).classList.add('imgCaseActive')
+              document.getElementById(x+'-'+y).classList.remove('imgCase')
+              document.getElementById(x+'-'+y).classList.add('imgCaseActive')
+              if(jeu.gameWin()){
+                document.getElementById("memory__result").classList.add("win");
+                document.getElementById("memory__result").textContent = "Félicitation c'est gagné.";
+                jeu.stopCompteur();
+              }
+            }else{
+              document.getElementById(x+'-'+y).setAttribute('src', './img/mem_back.jpg');
+              document.getElementById(caseReveal.returnX()+'-'+caseReveal.returnY()).setAttribute('src', './img/mem_back.jpg');
+            }
+            jeu.setReveal(0);
+          }, 1000)
+        }
+      }
+    }
+  }
+}
 /**
  * Method for create array game and add that in HTML page.
  * @param {Game} jeu 
  */
 function createArrayGame(jeu){
-  // test = document.getElementById("memory__jeu");
   for (let i = 0; i < tailleJeu; i++) {
-    newLigne = [];
     for (let j = 0; j < tailleJeu; j++) {
       jeu.addCase(new Case("mem"+(parseInt((i * tailleJeu + j) / 2 + 1))+".jpg", i, j));
     }
   }
-  // jeu.afficheArray();
+  mixTable(jeu);
+  createLigneHTML(jeu.returnArray(), jeu);
 }
 
-function main(){
-  jeu = new Game(this.tailleJeu);
-  createArrayGame(jeu);
+function restartGame(){
+  for (let i = 0; i < this.tailleJeu; i++) {
+    for (let j = 0; j < this.tailleJeu; j++) {
+      document.getElementById(i+'-'+j).src = './img/mem_back.jpg';
+      document.getElementById(i+'-'+j).classList.remove('imgCaseActive');
+      document.getElementById(i+'-'+j).classList.add('imgCase');
+    }
+    document.getElementById("memory__result").textContent = "";
+    document.getElementById("memory__result").classList.remove("win", "loose")
+  }
+  mixTable(this.jeu);
+  this.jeu.reinit();
 }
+
+function main(jeu){
+  this.jeu = new Game(this.tailleJeu);
+  createArrayGame(this.jeu);
+  console.log(this.jeu.arrayCase);
+}
+
+
+// if(jeu.returnCase(x,y).hidden == false){
+// }
+// if(jeu.returnCase(x,y).hidden == false && jeu.getCaseReveal() && jeu.getCaseReveal().hidden == false){
+//   if(nbReveal == 1){
+//     setTimeout(() => {
+//       let caseReveal = jeu.getCaseReveal();
+//       if(!(jeu.returnCase(x,y).returnName() === caseReveal.returnName())){
+//         document.getElementById(x+'-'+y).setAttribute('src', './img/mem_back.jpg');
+//         document.getElementById(caseReveal.returnX()+'-'+caseReveal.returnY()).setAttribute('src', './img/mem_back.jpg');
+//         jeu.setReveal(0);
+//       }else{
+//         jeu.returnCase(x,y).hidden = true;
+//         caseReveal.hidden = true;
+//         jeu.setReveal(0);
+//       }
+//       jeu.editCaseReveal(null);
+//     }, 1000);
+//   }
+// }else{
+//   jeu.setReveal(jeu.returnNbReveal() + 1);
+//   jeu.editCaseReveal(jeu.returnCase(x,y));
+// }
